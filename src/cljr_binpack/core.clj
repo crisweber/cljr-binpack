d1139fa0fb0eda3e87f65f66148f6bf24f0bd917
Cristofer <cris.weber@gmail.com>
2011-08-10 21:56:08 -0300

    some english corrections

--- src/cljr_binpack/core.clj
+++ src/cljr_binpack/core.clj
@@ -1,17 +1,17 @@
 (ns cljr-binpack.core)
 
 (defn fit-in-bin?
-  "Predicate to evaluate if an item fits in the bin provided"
+  "Predicate to evaluate if an item fits provided bin"
   [item bin bin-size]
   (<= (+ (apply + bin) item) bin-size))
 
 (defn split-on-fittable-bin
-  "Splits the bins sequence in 2 other sequences: The first where item don't fit and the other where item fit"
+  "Splits the bins sequence in 2 other sequences: The first where the item won't fit and the other where item must fit"
   [item bins bin-size]
   (split-with (fn [bin] (not (fit-in-bin? item bin bin-size))) bins))
 
 (defn first-available-bin
-  "Pack item inside first available bin where item fits. If there's no bin available, a new bin will be opened."
+  "Packs the item on the first box where it fits. If there's no bin available, a new bin will be opened."
   [bin-size bins item]
   (let [[filled-bins, [bin-to-append & remaining-bins]]  (split-on-fittable-bin item bins bin-size)]
     (filter (comp not empty?)
